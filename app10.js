fdbd3cf79fcba9c9c4d22c699482836c319b13f4
James-Frown <21122@ctca.co.za>
2022-08-03 13:50:48 +0200

    Objects & Arrays
    Finding some fun use cases

---
+++ app10.js
@@ -0,0 +1,22 @@
+let log = console.log;
+
+const test1 = {
+    name: "james",
+    surName: "brown"
+}
+
+const test2 = [
+    ["name", "dean"],
+    ["surName", "machine"]
+]
+
+const classRoom1 = [test1, test2];
+
+const classRoom2 = {
+    Students: {
+        test1,
+        test2[0, 1]
+    }
+}
+
+log(classRoom2);
---
+++ app9.js
@@ -0,0 +1,16 @@
+// object 
+
+let log = console.log;
+
+const test1 = {
+    name: "james",
+    surName: "brown"
+}
+
+const test2 = [
+    ["name", "dean"],
+    ["surName", "machine"]
+]
+
+log(1, test1);
+log(2, test2);
