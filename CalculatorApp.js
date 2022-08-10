c4be10c41153d41842a2b9d153f776c87c1ff7b7
James-Frown <21122@ctca.co.za>
2022-08-10 17:48:51 +0200

    just an update
    ...

--- CalculatorApp.js
+++ CalculatorApp.js
@@ -1,9 +1,39 @@
-let log = console.log();
+let myCalculation = [];
 
-const inputA = "";
-
-function inputs() {
-    inputA += prompt("Enter a word");
+function addNumber(number) {
+    myCalculation.push(number);
+    console.log(myCalculation);
+    return myCalculation;
 }
 
-log(inputA);
+function addOperator(operator) {
+    myCalculation.push(operator);
+    console.log(myCalculation);
+    return myCalculation;
+}
+
+// get result
+function getSum() {
+    let sum = 0;
+    for (let i = 0; i < myCalculation.length; i++) {
+        sum += myCalculation[i];
+        console.log(sum);
+    }
+    return sum;
+}
+
+function getSubtracted() {
+    let sum = 0;
+    for (let i = myCalculation.length - 1; i >= 0; i--) {
+        sum += myCalculation[i];
+        console.log(sum);
+    }
+    return sum;
+}
+
+addNumber(1);
+addNumber(1);
+addNumber(1);
+addNumber(1);
+getSum();
+getSubtracted();
